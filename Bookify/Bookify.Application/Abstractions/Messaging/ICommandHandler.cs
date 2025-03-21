c6c14d5cd7cd47825d26bf723fb3ab0f82562514
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2025-03-21 08:12:39 +0530

    Adding the required abstractions for CQRS
    Adding the required abstractions for CQRS

---
+++ Bookify/Bookify.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions.Messaging
+{
+    public interface ICommandHandler<TCommand> : IRequestHandler<TCommand,Result>
+        where TCommand : ICommand
+    {
+    }   
+    public interface ICommandHandler<TCommand,TReponse> : IRequestHandler<TCommand,Result<TReponse>>
+        where TCommand : ICommand<TReponse>
+    {
+    }
+}
