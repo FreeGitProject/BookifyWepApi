3862c1d0eba84c4b30d026bc4f62ef17822b381f
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2025-03-22 22:10:31 +0530

    Implementing Permission-based Authorization
    Implementing Permission-based Authorization

--- Bookify/Bookify.Api/Controllers/Users/UsersController.cs
+++ Bookify/Bookify.Api/Controllers/Users/UsersController.cs
@@ -2,6 +2,7 @@
 using Bookify.Application.Users.GetLoggedInUser;
 using Bookify.Application.Users.LogInUser;
 using Bookify.Application.Users.RegisterUser;
+using Bookify.Infrastructure.Authorization;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +21,7 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet("me")]
+    [HasPermission(Permissions.UsersRead)]
     public async Task<IActionResult> GetLoggedInUser(CancellationToken cancellationToken)
     {
         var query = new GetLoggedInUserQuery();
