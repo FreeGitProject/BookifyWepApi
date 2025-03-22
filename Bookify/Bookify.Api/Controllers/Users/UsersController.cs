c44a0a951272d67c3bb1170af4a19aa5962fc113
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2025-03-22 20:02:07 +0530

    Implementing the user login feature
      Implementing the user login feature

--- Bookify/Bookify.Api/Controllers/Users/UsersController.cs
+++ Bookify/Bookify.Api/Controllers/Users/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Bookify.Application.Users.RegisterUser;
+﻿//using Bookify.Application.Users.GetLoggedInUser;
+using Bookify.Application.Users.LogInUser;
+using Bookify.Application.Users.RegisterUser;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -48,21 +50,21 @@ public class UsersController : ControllerBase
         return Ok(result.Value);
     }
 
-    //[AllowAnonymous]
-    //[HttpPost("login")]
-    //public async Task<IActionResult> LogIn(
-    //    LogInUserRequest request,
-    //    CancellationToken cancellationToken)
-    //{
-    //    var command = new LogInUserCommand(request.Email, request.Password);
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> LogIn(
+        LogInUserRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new LogInUserCommand(request.Email, request.Password);
 
-    //    var result = await _sender.Send(command, cancellationToken);
+        var result = await _sender.Send(command, cancellationToken);
 
-    //    if (result.IsFailure)
-    //    {
-    //        return Unauthorized(result.Error);
-    //    }
+        if (result.IsFailure)
+        {
+            return Unauthorized(result.Error);
+        }
 
-    //    return Ok(result.Value);
-    //}
+        return Ok(result.Value);
+    }
 }
--- Bookify/Bookify.Infrastructure/DependencyInjection.cs
+++ Bookify/Bookify.Infrastructure/DependencyInjection.cs
@@ -31,30 +31,7 @@ public static class DependencyInjection
         services.AddTransient<IEmailService, EmailService>();
 
         AddPersistence(services, configuration);
-        services
-           .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-           .AddJwtBearer();
-        services.Configure<AuthenticationOptions>(configuration.GetSection("Authentication"));
-
-        services.ConfigureOptions<JwtBearerOptionsSetup>();
-        services.Configure<KeycloakOptions>(configuration.GetSection("Keycloak"));
-
-        services.AddTransient<AdminAuthorizationDelegatingHandler>();
-
-        services.AddHttpClient<IAuthenticationService, AuthenticationService>((serviceProvider, httpClient) =>
-        {
-            var keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
-
-            httpClient.BaseAddress = new Uri(keycloakOptions.AdminUrl);
-        })
-        .AddHttpMessageHandler<AdminAuthorizationDelegatingHandler>();
-
-        services.AddHttpClient<IJwtService, JwtService>((serviceProvider, httpClient) =>
-        {
-            var keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
-
-            httpClient.BaseAddress = new Uri(keycloakOptions.TokenUrl);
-        });
+        AddAuthentication(services, configuration);
         return services;
     }
     private static void AddPersistence(IServiceCollection services, IConfiguration configuration)
@@ -81,4 +58,38 @@ public static class DependencyInjection
 
         SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
     }
+    private static void AddAuthentication(IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer();
+
+        services.Configure<AuthenticationOptions>(configuration.GetSection("Authentication"));
+
+        services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+        services.Configure<KeycloakOptions>(configuration.GetSection("Keycloak"));
+
+        services.AddTransient<AdminAuthorizationDelegatingHandler>();
+
+        services.AddHttpClient<IAuthenticationService, AuthenticationService>((serviceProvider, httpClient) =>
+        {
+            var keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
+
+            httpClient.BaseAddress = new Uri(keycloakOptions.AdminUrl);
+        })
+            .AddHttpMessageHandler<AdminAuthorizationDelegatingHandler>();
+
+        services.AddHttpClient<IJwtService, JwtService>((serviceProvider, httpClient) =>
+        {
+            var keycloakOptions = serviceProvider.GetRequiredService<IOptions<KeycloakOptions>>().Value;
+
+            httpClient.BaseAddress = new Uri(keycloakOptions.TokenUrl);
+        });
+
+       // services.AddHttpContextAccessor();
+
+        //services.AddScoped<IUserContext, UserContext>();
+    }
+
 }
