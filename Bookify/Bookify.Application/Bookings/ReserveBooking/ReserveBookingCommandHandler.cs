254696cd6b8975bf5b7b4a912a78e8d0307a0f02
Avinash.Gupta <avinash.gupta@bettercommerce.io>
2025-03-22 15:28:23 +0530

    Bookify-Infrastructure
    Bookify-Infrastructure

--- Bookify/Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
+++ Bookify/Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -4,6 +4,7 @@ using Bookify.Domain.Abstractions;
 using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings;
 using Bookify.Domain.Users;
+using System.Data;
 
 namespace Bookify.Application.Bookings.ReserveBooking;
 
@@ -55,17 +56,26 @@ internal sealed class ReserveBookingCommandHandler : ICommandHandler<ReserveBook
             return Result.Failure<Guid>(BookingErrors.Overlap);
         }
 
-        var booking = Booking.Reserve(
-            apartment,
-            user.Id,
-            duration,
-            _dateTimeProvider.UtcNow,
-            _pricingService);
+        try
+        {
+            var booking = Booking.Reserve(
+                apartment,
+                user.Id,
+                duration,
+                _dateTimeProvider.UtcNow,
+                _pricingService);
 
-        _bookingRepository.Add(booking);
+            _bookingRepository.Add(booking);
 
-        await _unitOfWork.SaveChangesAsync(cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-        return booking.Id;
+            return booking.Id;
+
+        }
+        catch (DBConcurrencyException)
+        {
+            return Result.Failure<Guid>(BookingErrors.Overlap);
+        }
+       
     }
 }
